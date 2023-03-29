4add07ef5cad63fa62808d126a7b1bfbdf3ebdba
Ludeo <lucas.steuer1@gmx.de>
2023-03-29 23:14:41 +0200

    fixed navigation issue with tables

--- src/components/tables/tableFooter.jsx
+++ src/components/tables/tableFooter.jsx
@@ -1,17 +1,38 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react";
 
 import styles from "./tableFooter.module.css";
 
 const TableFooter = ({ range, setPage, page, slice }) => {
+    const [actualRange, setActualRange] = useState([]);
+
     useEffect(() => {
         if (slice.length < 1 && page !== 1) {
             setPage(page - 1);
         }
-    }, [slice, page, setPage]);
+
+        let tempRange = [];
+        let startPoint = parseInt(page) - 5;
+        let endPoint = startPoint + 10;
+
+        if(startPoint < 0) {
+            startPoint = 0;
+            endPoint = 10;
+        } else if(endPoint > range.length) {
+            endPoint = range[range.length - 1];
+        }
+
+        while(startPoint < endPoint) {
+            tempRange.push(range[startPoint]);
+            startPoint++;
+        }
+
+        setActualRange(tempRange);
+    }, [slice, page, setPage, range]);
+    
 
     return (
         <div className={styles.tableFooter}>
-            {range.map((el, index) => (
+            {actualRange && actualRange.map((el, index) => (
                 <button
                     key={index}
                     className={`${styles.button} ${
